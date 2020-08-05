0bb907a424fed31072764c3b43b91de53b958898
Vitali <nva87k@gmail.com>
2020-08-05 21:34:57 +0300

    Part 19 is finished

---
+++ BookHelloWorld/Assets/CubeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+
+{
+    public GameObject cubePrefabvar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*Instantiate(cubePrefabvar);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(cubePrefabvar);
+    }
+}
