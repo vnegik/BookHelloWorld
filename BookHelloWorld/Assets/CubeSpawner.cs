f816058099905085aa269da9eb0b13d75bdb292c
Vitali Nikalaenka <vnikalaenka@openmygame.com>
2020-10-02 15:41:32 +0300

    С винды

--- BookHelloWorld/Assets/CubeSpawner.cs
+++ BookHelloWorld/Assets/CubeSpawner.cs
@@ -6,10 +6,22 @@ public class CubeSpawner : MonoBehaviour
 
 {
     public GameObject cubePrefabvar;
+    public float one;
+    public int two;
+    public int three;
+
     // Start is called before the first frame update
     void Start()
     {
         /*Instantiate(cubePrefabvar);*/
+        one = Screen.dpi;
+        print(one);
+        two = Screen.width;
+        print(two);
+        three = Screen.height;
+        print(three);
+        print(SystemInfo.operatingSystem);
+        print("Test");
     }
 
     // Update is called once per frame
